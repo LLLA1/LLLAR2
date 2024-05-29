abb79b79f3dddb0e5b5346272ec12b834fb08ac7
fasdxoox <163231340+fasdxoox@users.noreply.github.com>
2024-05-29 17:56:12 +0300

    Update الإشتراك الاجباري.py

--- ZeMusic/plugins/play/الإشتراك الاجباري.py
+++ ZeMusic/plugins/play/الإشتراك الاجباري.py
@@ -2,8 +2,9 @@ from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
 from pyrogram.errors import ChatAdminRequired, UserNotParticipant, ChatWriteForbidden
 from ZeMusic import app
+import config
 
-Muntazer ="QQQ_Q50"
+Muntazer = "Muntazer"
 @app.on_message(filters.incoming & filters.private, group=-1)
 async def must_join_channel(app: Client, msg: Message):
     if not Muntazer:
@@ -22,7 +23,7 @@ async def must_join_channel(app: Client, msg: Message):
                     f"~︙عزيزي {msg.from_user.mention} \n~︙عليك الأشتراك في قناة البوت \n~︙قناة البوت : @{Muntazer}.",
                     disable_web_page_preview=True,
                     reply_markup=InlineKeyboardMarkup([
-                        [InlineKeyboardButton("• 𝐒𝐨𝐮𝐫𝐜𝐞 𝐋𝐚𝐫𝐢𝐧 🎧", url=f"https://t.me/SOURCELARIN")]
+                        [InlineKeyboardButton(text=config.LARIN_NAME, url=config.LARIN_LINK)]
                     ])
                 )
                 await msg.stop_propagation()
